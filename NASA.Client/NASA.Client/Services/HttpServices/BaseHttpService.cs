f633a92fb67551c927e91719485f660680fe6dfd
Blockiy_Dmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2022-10-20 09:13:56 +0300

    Update rest api

--- NASA.Client/NASA.Client/Services/HttpServices/BaseHttpService.cs
+++ NASA.Client/NASA.Client/Services/HttpServices/BaseHttpService.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using NASA.Client.Services.LoggerServices;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace NASA.Client.Services.HttpServices
 {
     public abstract class BaseHttpService
     {
+        #region Uri
+
+        #endregion 
+        protected ILogService _logService { get; set; }
+        protected IConfiguration _configuration { get; set; }
+        protected HttpClient _httpClient { get; set; }
+        protected JsonSerializerOptions _options { get; set; }
+
+        public BaseHttpService(ILogService logService, IConfiguration configuration, HttpClient httpClient)
+        {
+            _logService = logService;
+            _configuration = configuration;
+
+            _httpClient = httpClient;
+
+            _options = new JsonSerializerOptions()
+            {
+                ReferenceHandler = ReferenceHandler.Preserve,
+                PropertyNameCaseInsensitive = true
+            };
+
+            InitUrlFromConfig();
+        }
+        private void InitUrlFromConfig()
+        {
+            GetCurrentCustomer = _configuration["Api:User:GetCurrentCustomer"];
+        }
     }
 }
